46de9795f20f266f76f37fec826ac2ee0aa0ba47
anne <141642305+annexistente@users.noreply.github.com>
2024-10-24 07:54:29 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,36 +1,36 @@
-criaCartao{
+criaCartao(
     'Mamma Mia',
     'Qual o nome da atriz da Donna?',
     'Meryl Streep',
-}
+)
 
-criaCartao{
+criaCartao(
     'Mamma Mia',
     'O que Sophie quer descobrir?',
     'Quem é o pai dela',
-}
+)
 
-criaCartao{
+criaCartao(
     'Dress to impress',
     'Quem criou o Dress to impress?',
     'Gigi',
-}
+)
 
-criaCartao{
+criaCartao(
     'Dress to impress',
     'Quanto ela ganhou com o jogo até agora?',
     '2 milhões',
-}
+)
 
-criaCartao{
+criaCartao(
     'UFPR',
     'Quem criou a UFPR?',
     'Victor Ferreira do Amaral',
-}
+)
 
-criaCartao{
+criaCartao(
     'UFPR',
     'Qual o curso mais disputado?',
     'Medicina',
-}
+)
 
